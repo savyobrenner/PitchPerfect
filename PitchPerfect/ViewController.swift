30c71d0e010534f2b602c6cfad40f055771485a2
Savyo Brenner <savyobrenner@gmail.com>
2021-08-04 00:36:46 -0300

    add assets

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        isRecording(false)
         // Do any additional setup after loading the view.
     }
+    
+    private func isRecording(_ validator: Bool) {
+        stopRecordingButton.isEnabled = validator
+        recordButton.isEnabled = !validator
+        recordingLabel.text = validator ? "Recording in Progress" : "Tap to Record"
+    }
 
+    @IBAction func recordAudio(_ sender: UIButton) {
+        isRecording(true)
+    }
+    
+    @IBAction func stopRecording(_ sender: UIButton) {
+        isRecording(false)
+    }
+    
 
 }
 
